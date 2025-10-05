c490776f1a8850a96cf133e0c6ffa13400ac55f1
Puneendra <samanapuneendra@gmail.com>
2025-10-05 10:30:49 +0530

    Puni1 (#7)
    * added ExcelOperations
    
    * Exceptions added
    
    * added

--- src/main/java/Exceptions/NullPointerException.java
+++ src/main/java/Exceptions/NullPointerException.java
@@ -1,4 +1,19 @@
 package Exceptions;
 
-public class NullPointerException {
+public class NullPointerException
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            String s ="puni";
+            s=null;
+            System.out.println(s.length());
+        }
+        catch(Exception e)
+            {
+            System.out.println(e);
+            }
+
+    }
 }
--- src/main/java/Exceptions/NumberFormatException.java
+++ src/main/java/Exceptions/NumberFormatException.java
@@ -1,4 +1,21 @@
 package Exceptions;
 
-public class NumberFormatException {
+public class NumberFormatException
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            int a= 20;
+            String b= "puni";
+
+            System.out.println(a+b);
+            System.out.println(Integer.parseInt(a+b));
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+
+    }
 }
