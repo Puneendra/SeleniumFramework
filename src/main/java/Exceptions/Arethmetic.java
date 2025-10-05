5028a95ed7863e3195f8cf0fd9735981a3f376f1
Puneendra <samanapuneendra@gmail.com>
2025-10-05 10:13:39 +0530

    added

--- src/main/java/Exceptions/Arethmetic.java
+++ src/main/java/Exceptions/Arethmetic.java
@@ -1,4 +1,24 @@
 package Exceptions;
 
-public class Arethmetic {
+public class Arethmetic
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            int a = 10;
+            int b=10/0;
+            System.out.println(b);
+
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);//e.getMessage()
+        }
+        finally
+        {
+            System.out.println("successfully executed ");
+        }
+
+    }
 }
--- src/main/java/Exceptions/ArrayIndex.java
+++ src/main/java/Exceptions/ArrayIndex.java
@@ -1,4 +1,23 @@
 package Exceptions;
 
-public class ArrayIndex {
+public class ArrayIndex
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            int arr[]= {1,2,3,4,5};
+
+            System.out.println(arr[10]);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);  //e.getMessage();
+        }
+        finally
+        {
+            System.out.println("i love java ");
+        }
+
+    }
 }
