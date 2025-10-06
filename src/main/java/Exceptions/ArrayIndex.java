d1730fda52313973040d2f6732df45904ca4c98b
Puneendra <samanapuneendra@gmail.com>
2025-10-06 14:45:01 +0530

    array (#8)

--- src/main/java/Exceptions/ArrayIndex.java
+++ src/main/java/Exceptions/ArrayIndex.java
@@ -1,4 +1,28 @@
 package Exceptions;
 
+import java.sql.SQLOutput;
+
 public class ArrayIndex {
+    public static void main(String[] args)
+    {
+
+        try
+        {
+            int arr[]={1,2,3,4,5,6,7};
+            System.out.println(arr[10]);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        finally
+
+        {
+            System.out.println("printing from finally block");
+        }
+
+    }
+
+
+
 }
